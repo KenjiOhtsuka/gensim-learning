353de05773d46f98d9b42d1ad394e776ebb09be0
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-08-19 12:34:54 +0900

    add gitignore

--- tutorial_3.py
+++ tutorial_3.py
@@ -1,5 +1,6 @@
 from os import path
 import logging
+from gensim import corpora, models, similarities
 
 
 def temp_file_path(relative_path):
@@ -8,20 +9,27 @@ def temp_file_path(relative_path):
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-from gensim import corpora, models, similarities
-
 dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
 corpus = corpora.MmCorpus('/tmp/deerwester.mm')  # comes from the first tutorial, "From strings to vectors"
 print(corpus)
 
+################################################################################
+# Create 2-dimentional LSI space
+################################################################################
 lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
 
+################################################################################
+# Judge similarity of a text
+################################################################################
 doc = "Human computer interaction"
 vec_bow = dictionary.doc2bow(doc.lower().split())
 vec_lsi = lsi[vec_bow]  # convert the query to LSI space
 print(vec_lsi)
 
-index = similarities.MatrixSimilarity(lsi[corpus])  # transform corpus to LSI space and index it
+################################################################################
+# transform corpus to LSI space and index it
+################################################################################
+index = similarities.MatrixSimilarity(lsi[corpus])
 
 index.save(temp_file_path('deerwester.index'))
 index = similarities.MatrixSimilarity.load(temp_file_path('deerwester.index'))
